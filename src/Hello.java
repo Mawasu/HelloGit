42b2f738bf8f65ecf86472802d60e4827789f73e
Malas <73205524+Mawasu@users.noreply.github.com>
2023-08-24 12:02:01 -0500

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Cale Sigerson
+ *         EMAIL
+ * Date: 2023/08/24
+ *
+ * A simple hello world program in Java
+ *
+ */
+public class Hello {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Cale Sigerson\nComputer Science");
+    }
+}
